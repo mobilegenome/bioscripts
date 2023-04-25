c29ebea414e719277edb1c14f43d5eca7a18afea
Fritjof <fritjof.lammers@vogelwarte.ch>
2023-04-25 11:08:08 +0200

    add dataframe approach for merging CpG site

--- get_CpG_from_genome/get_CpG_from_genome.py
+++ get_CpG_from_genome/get_CpG_from_genome.py
@@ -16,6 +16,7 @@ coverage information for these sites.
 
 import argparse
 import os
+import re
 import numpy as np
 import pandas as pd
 from Bio import SeqIO
@@ -66,26 +67,97 @@ def get_sequence(bed_chunk: np.array, direction: str = "+1") -> list:
     sequence = map(lambda _seq, _start, _end: genome[_seq][_start:_end], seqnames, starts, ends)
     return list(sequence)
 
+
+def process_genome(bed_chunks: list) -> None:
+    n_chunk = len(bed_chunks)
+    print("start processing")
+
+    for chunk_no, chunk in enumerate(bed_chunks):
+        print(f"processing chunk no {chunk_no+1} / {n_chunk}", end="\r")
+        for _direction in DIRECTIONS:
+            chunk_seqrecords = get_sequence(chunk[["seqname", "start", "end"]].values, direction=_direction)
+            chunk_sequences = list(map(lambda x: str(x.seq), chunk_seqrecords))
+            chunk.loc[:, f"REF_{_direction}"] = chunk_sequences
+
+        chunk.to_csv(args.output_bed, mode='a',
+                     header=not os.path.exists(args.output_bed), sep="\t", index=False,
+                     )
+    print(f"finished. wrote annotated file to {args.output_bed}")
+
+
 # split the BED dataframe into chunks of specified size
 chunk_size = args.chunk_size
 bed_chunks = [bed[i:i+chunk_size] for i in range(0, len(bed), chunk_size)]
 
 # extract DNA sequences for each BED chunk
-
 if os.path.exists(args.output_bed):
     print("Error: Outputfile exists!")
     quit()
 
-n_chunk = len(bed_chunks)
-print("start processing")
-for chunk_no, chunk in enumerate(bed_chunks):
-    print(f"processing chunk no {chunk_no+1} / {n_chunk}", end="\r")
-    for _direction in DIRECTIONS:
-        chunk_seqrecords = get_sequence(chunk[["seqname", "start", "end"]].values, direction=_direction)
-        chunk_sequences = list(map(lambda x: str(x.seq), chunk_seqrecords))
-        chunk.loc[:, f"REF_{_direction}"] = chunk_sequences
+process_genome(bed_chunks)
+# done
 
-    chunk.to_csv(args.output_bed, mode='a',
-                 header=not os.path.exists(args.output_bed), sep="\t", index=False,
-                 )
-print(f"finished. wrote annotated file to {args.output_bed}")
+
+bed = pd.read_table("get_CpG_from_genome/test/output_tri.bed.gz")
+df[df.isna().any(axis=1)]
+bed.dropna(inplace=True)
+# define a function to extract DNA sequences
+
+def merge_reverse_strand_calls(df, context_column_label="REF_-1+1"):
+
+    if context_column_label not in df.columns:
+        print(f"Error. Column {context_column_label} not found in DataFrame.")
+
+    dfs_split = {
+        "forward": df[df["REF_-1+1"].str.endswith("CG")],
+        "reverse": df[df["REF_-1+1"].str.startswith("CG")]
+    }
+
+
+    map(lambda df: df.drop("perc_mCpG", axis=1, inplace=True), dfs_split.values]
+# filter and subset for testing
+df_cpg_fw, df_cpg_rev = [df[df["seqname"] == "chr25"] for df in [df_cpg_fw, df_cpg_rev]]
+df_cpg_fw, df_cpg_rev = [df.head() for df in [df_cpg_fw, df_cpg_rev]]
+
+df_cpg_rev["start-1"] = df_cpg_rev["start"] -1
+
+df_cpg_merged = df_cpg_fw.merge(df_cpg_rev,
+                                how="left",
+                                left_on=["seqname", "start"],
+                                right_on=["seqname", "start-1"],
+                                suffixes=["_fw", "_rev"])
+
+df_cpg_merged["numCs"] = df_cpg_merged["numCs_fw"] +df_cpg_merged["numCs_rev"]
+df_cpg_merged["numTs"] = df_cpg_merged["numTs_fw"] +df_cpg_merged["numTs_rev"]
+
+df_cpg_merged["perc_mCpG"] = \
+    df_cpg_merged["numCs"] / (df_cpg_merged["numCs"] + df_cpg_merged["numTs"]) * 100
+
+column_filter = re.compile(".*_fw$|.*_rev$")
+column_filter_list = list(filter(column_filter.search, df_cpg_merged.columns))
+df_cpg_merged.drop(column_filter_list, axis=1, inplace = True)
+
+del df_cpg_merged
+
+for idx, row in bed.iterrows():
+    seqname = row["seqname"]
+    pos = row["start"]
+    context = row["REF_-1+1"]
+
+    # is a forward CpG site? then nothing to do
+    row_cpg = df_cpg[(df_cpg["seqname"] == seqname) & (df_cpg["start"] == pos)]
+
+    # is a reverse CpG site
+    if context.startswith("CG"):
+        # and previous site is
+        row_cpg_previous = df_cpg[(df_cpg["seqname"] == seqname) & (df_cpg["start"] == pos-1)]
+        row_loc = (df_cpg["seqname"] == seqname) & (df_cpg["start"] == pos - 1)
+        if not row_cpg_previous.empty:
+            df_cpg.loc[row_loc, ["numCs", "numTs"]] += row["numCs"], row["numTs"]
+            df_cpg.loc[row_loc, "perc_mCpG"] = \
+                df_cpg.loc[row_loc, "numCs"] / (df_cpg.loc[row_loc, "numCs"] + df_cpg.loc[row_loc, "numTs"]) * 100
+        else:
+            df_cpg[row_loc] = row
+
+
+df_cpg.to_csv(args.output_bed.replace(".bed.", ".CpG_forward.bed."), sep="\t", header=False, index=False, float_format="%.5f")
