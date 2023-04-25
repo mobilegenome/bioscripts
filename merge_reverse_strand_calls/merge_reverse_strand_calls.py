1c9404c18a8e33625c024066a39e5b89509d461a
Fritjof <fritjof.lammers@vogelwarte.ch>
2023-04-25 15:43:42 +0200

    working version, but leaves out nonmatching sites

---
+++ merge_reverse_strand_calls/merge_reverse_strand_calls.py
@@ -0,0 +1,81 @@
+import argparse
+import os
+import re
+import numpy as np
+import pandas as pd
+
+pd.options.mode.chained_assignment = None  # default='warn'
+
+
+COORDINATE_BASE = 1  # 1 for Bismark coverage file, 0 for BED file
+# set to the following to get upstream and downstream information separately ["+1", "-1", "-1+1"]:
+DIRECTIONS = ["-1+1"]
+
+# define command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-b", "--bed_file", help="path to the input BED file")
+parser.add_argument("-o", "--output_bed", help="path to the output BED file")
+
+args = parser.parse_args()
+
+
+
+def merge_reverse_strand_calls(df: pd.DataFrame,
+                               context_column_label: str = "REF_-1+1") -> pd.DataFrame:
+
+    # find and report any NaNs in data
+    _df_nan = df[df.isna().any(axis=1)]
+    if not _df_nan.empty:
+        print("Warning: the following elements of the DataFrame contain NaNs")
+        print(_df_nan)
+        df = df.dropna()
+
+    if context_column_label not in df.columns:
+        print(f"Error. Column {context_column_label} not found in DataFrame.")
+
+    print("prepare dataframs")
+
+    dfs_split = {
+        "forward": df[df["REF_-1+1"].str.endswith("CG")],
+        "reverse": df[df["REF_-1+1"].str.startswith("CG")]
+    }
+
+    # drop unused perc_mCpG column
+    dfs_split = {_label: _df.drop("perc_mCpG", axis=1) for _label, _df in dfs_split.items()}
+
+    dfs_split["reverse"]["start-1"] = dfs_split["reverse"]["start"] - 1
+
+    print("joining dataframes")
+    df_merged = dfs_split["forward"].merge(dfs_split["reverse"],
+                                                how="left",
+                                                left_on=["seqname", "start"],
+                                                right_on=["seqname", "start-1"],
+                                                suffixes=["_fw", "_rev"])
+
+    print("adding read counts")
+    df_merged["numCs"] = df_merged.numCs_fw + df_merged.numCs_rev
+    df_merged["numTs"] = df_merged.numTs_fw + df_merged.numTs_rev
+
+
+    print("calculate percentage mCpG")
+    df_merged["perc_mCpG"] = df_merged.numCs / (df_merged.numCs + df_merged.numTs) * 100
+    df_merged["start"] = df_merged["start-1"].astype('Int64')
+    df_merged["end"] = df_merged["start-1"].astype('Int64')
+
+    print("drop unused columns")
+    _column_filter = re.compile(".*_fw$|.*_rev$")
+    _column_filter_list = list(filter(_column_filter.search,
+                                      df_merged.columns)) + ["start-1"]
+
+    df_merged.drop(_column_filter_list, axis=1, inplace=True)
+    df_merged = df_merged[["seqname", "start", "end", "perc_mCpG", "numCs", "numTs"]]
+
+    return df_merged
+
+
+#args = parser.parse_args("-b get_CpG_from_genome/test/output_tri.bed.gz -o merge_reverse_strand_calls/test/output.bed".split(" "))
+
+df = pd.read_table(args.bed_file)
+df2 = merge_reverse_strand_calls(df)
+print(f"save dataframe with dimensions {df2.shape} to file")
+df2.to_csv(args.output_bed, sep="\t", index=False, header=False, float_format='%.3f')
